e5af8d9604d2029ee81b40d99ed61f6eb39484f4
Abdo Elkateb <elkatebdev@gmail.com>
2023-06-23 20:26:58 +0200

    update

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ let product = document.getElementById("product"),
   submit = document.getElementById("submit"),
   form = document.getElementById("form"),
   table = document.getElementById("table"),
-  datePro = JSON.parse(localStorage.getItem("product")) ?? [];
+  datePro = JSON.parse(localStorage.getItem("product")) ?? [],
+  status = null;
 
 datePro.forEach(function (item) {
   table.innerHTML += `
@@ -18,25 +19,45 @@ datePro.forEach(function (item) {
 });
 
 submit.addEventListener("click", (e) => {
-  // this code make it you to can sent value from tbody
   e.preventDefault();
-  table.innerHTML += `
 
-           <tr>
-           <td>${product.value}</td>
-           <td><button> delete</button></td>
-           <td><button> update</button></td>
-     </tr> 
- `;
+  if(status == null) {
+    table.innerHTML += `
 
-  const listDate = {
-    id: parseInt(Math.random() * 10000),
-    title: product.value,
-  };
+            <tr>
+            <td>${product.value}</td>
+            <td><button> delete</button></td>
+            <td><button> update</button></td>
+              </tr>
+  `;
 
-  datePro.push(listDate);
-  localStorage.setItem("product", JSON.stringify(datePro));
-  listDate.value = "";
+    const listDate = {
+      id: parseInt(Math.random() * 10000),
+      title: product.value,
+    };
+
+    datePro.push(listDate);
+    localStorage.setItem("product", JSON.stringify(datePro));
+  } else {
+    let storage = JSON.parse(localStorage.getItem("product"))
+    let newDate = storage.map(function (item) {
+      if (item.id == status) {
+        return {
+          id:status,
+          title:product.value,
+        };
+       }else {
+        return item;
+       }
+    })
+    localStorage.setItem("product", JSON.stringify(newDate))
+    submit.value = "Add";
+    submit.style.background = "#fff";
+    submit.style.color = "#333";
+
+  } 
+product.value = ""
+
 });
 
 
@@ -62,13 +83,14 @@ deleteItom.forEach(function (item) {
 
 update.forEach(function(item) {
   item.addEventListener("click", ()=> {
-    console.log(item.parentElement.previousElementSibling.textContent)
     let taskes = item.parentElement.previousElementSibling.textContent
     product.value = taskes
     submit.value = "UPDATE";
     submit.style.background = "rgba(8, 248, 8, 0.829)";
     submit.style.border = "none",
       submit.style.color = "#fff";
+      status = item.getAttribute("data-id")
+      // console.log(status)
 
   
   })
